81444cdec46e1237a4b0264835d9e9e5e56c0b15
mltpoo <133835691+mltpoo@users.noreply.github.com>
2023-05-17 23:58:31 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+tatlongguhit = document.querySelector(".Threelines");
+tatlongguhit.onclick = function(){
+    navbar = document.querySelector(".navi");
+    navbar.classList.toggle("clicked");
+}
+
+alvie = document.querySelector(".cati");
+alvie.onclick = function(){
+    navbar = document.querySelector(".drop");
+    navbar.classList.toggle("pinindot");
+}
+
+john = document.querySelector(".madami");
+john.onclick = function(){
+    navbar = document.querySelector(".instrudrop");
+    navbar.classList.toggle("pinindotA");
+}
